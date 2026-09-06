aeeee98606cb0d8e51d01dd2d8cf589e16496fee
ykuete <y.kuete@yahoo.fr>
2026-09-06 16:27:16 -0600

    fixed test bug

--- __test__/appOperation.test.js
+++ __test__/appOperation.test.js
@@ -8,3 +8,7 @@ test('multiply 2 and 3 to equal 6', () => {
 test('add 2 and 3 to equal 5', () => {
     expect(appOperation.add(2, 3)).toBe(5);
 });
+
+test('subtract 3 from 5 to equal 2', () => {
+    expect(appOperation.subtract(5, 3)).toBe(2);
+});
